ce6787191043cbac9b3692aacc45d509d289a61b
Frederic Hoerni <fhoerni@free.fr>
2021-03-17 21:43:14 +0100

    Add 'value' analysis

--- visualize.py
+++ visualize.py
@@ -2,15 +2,19 @@
 # -*- coding: utf-8 -*-
 """Eventlog Visualizer
 
-Show a graph of events over time in either of two representations:
+Show a graph of events over time in either of these representations:
 
     - 'spot': events are represented as points
     - 'density': the number of events per period is represented as a curve
+    - 'value': a numeric value is represented as a curve
 
 Supported log format:
     LOG := LINE ...
     LINE := DATETIME SPACE TEXT EOL
 
+
+Example:
+    visualize.py --density info warning --spot err --value 'space: ([0-9]+)' -- test/sample.log
 """
 
 import sys
@@ -19,8 +23,9 @@ import datetime
 import re
 import matplotlib.pyplot as pyplot
 import matplotlib.dates as mdates
+import matplotlib.ticker as mticker
 
-VERSION = '1.1'
+VERSION = '1.2'
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 DENSITY_WINDOW_SIZE_S = 300 # 5 minutes
 
@@ -119,12 +124,103 @@ def get_spot_analysis(lines, pattern_spot):
 
     return spot_analysis
 
+
+def get_value_analysis(lines, pattern_value):
+    """Return the list of the dates and values.
+    
+    Arguments:
+        lines         : the lines of data
+        pattern_value : a REGEXP that must contain one group for capturing the numeric value
+    """
+    value_analysis = [] # [ (<datetime>, <value>) ]
+    pattern_value_re = re.compile(pattern_value)
+    for line in lines:
+        d, data = parse_line(line)
+        if d is None: continue
+
+        m = pattern_value_re.search(data)
+        if m:
+            try:
+                value_str = m.group(1)
+                value = float(value_str)
+                value_analysis.append( (d, value) )
+            except:
+                log_error('Cannot extract numeric (%s): %s' % (pattern_value, line))
+
+    return value_analysis
+
+
 def get_color(id):
     Colors = [ 'b', 'g', 'r', 'c', 'm', 'y', 'k' ]
     if id >= len(Colors): die('Not nough colors to represent data')
     return Colors[id]
 
-def display_graph(analysis_density, analysis_spot, title):
+
+def add_curve_scatter(axis, analysis_spot, color_idx):
+    """Ad one of more scatter curves that spot events
+
+    Arguments:
+        y_axis     : a pyplot x-y axis
+        analysis : a dictionnary { 'name': [<datetime>, ...], ... }
+    """
+
+    curves = []
+    # each spot analysis has a different y value
+    spot_value = 0
+    axis.set_ylim(-1, len(analysis_spot)+1)
+    axis.get_yaxis().set_visible(False)
+    
+    for name in analysis_spot:
+        data = analysis_spot[name]
+        color = get_color(color_idx)
+        t = data
+        data_spot = [spot_value for _x in data]
+        p = axis.scatter(t, data_spot, color=color, label=name)
+        curves.append(p)
+        spot_value += 1
+        color_idx += 1
+
+    return curves
+
+
+def add_curve_plot(axis, y_label, analysis, plot_style, color_idx, side):
+    """Ad one of more plot curves that display values
+
+    Arguments:
+        axis       : a pyplot x-y axis
+        y_label    : text to display on the y axis
+        analysis   : a dictionnary { 'name': [ (<datetime>, value) ] }
+        plot_style : eg. 'default', 'steps-post', ...
+        color_idx  : starting color index
+        side       : side of the y axis ('left' or right')
+    """
+    curves = []
+    axis.set_ylabel(y_label, color='k')
+    axis.tick_params(axis='y', labelcolor='k')
+    axis.yaxis.set_label_position(side)
+    if side == 'right':
+        axis.yaxis.tick_right()
+    else:
+        axis.yaxis.tick_left()
+
+    for name in analysis:
+        data = analysis[name]
+        color = get_color(color_idx)
+        t = [d for d, _x in data] # dates for the x axis
+        data_density = [dat for _x, dat in data] # values for the y axis
+        label = name + ' (' + side + ' y)'
+        marker = ''
+        if plot_style == 'default':
+            marker = '.'
+        p = axis.plot(t, data_density, color=color, label=label, drawstyle=plot_style,
+                      marker=marker)
+        curves += p
+        color_idx += 1
+
+    return curves
+
+
+def display_graph(title, analysis_density, analysis_spot, analysis_value):
     """Display a graph with the curve of the density and the spots.
     
     Arguments:
@@ -136,41 +232,40 @@ def display_graph(analysis_density, analysis_spot, title):
     """
 
     fig, ax1 = pyplot.subplots()
+    curves = []
 
     ax1.format_xdata = mdates.DateFormatter('%Y-%m-%d %H:%M')
-
-    color_idx = 0 # inreased for each dataset
-    # Density
     ax1.set_xlabel('datetime')
-    ax1.set_ylabel('density (%d s)' % (DENSITY_WINDOW_SIZE_S), color=get_color(color_idx))
-    for name in analysis_density:
-        data = analysis_density[name]
-        color = get_color(color_idx)
-        t = [d for d, _x in data] # dates for the x axis
-        data_density = [dat for _x, dat in data] # values for the y axis
-        ax1.plot(t, data_density, color=color, label=name, drawstyle='steps-post')
-        ax1.tick_params(axis='y', labelcolor=color)
-        ax1.legend(loc="upper left")
-        color_idx += 1
-    
+    ax1.get_yaxis().set_visible(False) 
+    color_idx = 0 # increased for each dataset
+
+    if len(analysis_density):
+        # Density
+        ax2 = ax1.twinx()  # instantiate another axis that shares the same x-axis
+        curves_plot = add_curve_plot(ax2, 'Density (%d s)' % (DENSITY_WINDOW_SIZE_S),
+                                     analysis_density, 'steps-post', color_idx, 'right')
+        curves += curves_plot
+        color_idx += len(curves_plot)
+        # As density values are integers, do not allow y axis to display decimal values.
+        ax2.yaxis.set_major_locator(mticker.MaxNLocator(integer=True))
+
+    if len(analysis_value):
+        # Density
+        ax2 = ax1.twinx()  # instantiate another axis that shares the same x-axis
+        curves_plot = add_curve_plot(ax2, 'Value', analysis_value, 'default', color_idx,
+                                     'left')
+        curves += curves_plot
+        color_idx += len(curves_plot)
+
     if len(analysis_spot):
         ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-        # each spot analysis has a different y value
-        spot_value = 0
-        ax2.set_ylim(-1, len(analysis_spot)+1)
-        ax2.get_yaxis().set_visible(False)
-    
-    for name in analysis_spot:
-        data = analysis_spot[name]
-        color = get_color(color_idx)
-        ax2.set_ylabel('spot', color=color)  # we already handled the x-label with ax1
-        t = data
-        data_spot = [spot_value for _x in data]
-        ax2.scatter(t, data_spot, color=color, label=name)
-        ax2.tick_params(axis='y', labelcolor=color)
-        ax2.legend(loc="upper right")
-        spot_value += 1
-        color_idx += 1
+        curves_scatter = add_curve_scatter(ax2, analysis_spot, color_idx)
+        curves += curves_scatter
+        color_idx += len(curves_scatter)
+
+    # Add legend
+    labels = [c.get_label() for c in curves]
+    ax1.legend(curves, labels, loc='best')
 
     # rotates and right aligns the x labels, and moves the bottom of the
     # axes up to make room for them
@@ -194,6 +289,8 @@ def main():
                         help='pattern(s) for density representation (RegEx)')
     parser.add_argument('-s', '--spot', nargs='+', metavar='PATTERN',
                         help='pattern(s) for spot representation (RegEx)')
+    parser.add_argument('-v', '--value', nargs='+', metavar='PATTERN',
+                        help='pattern(s) for value representation (RegEx)')
     parser.add_argument('-f', '--date-format',
                         help='Date format for strptime (default %s)' % 
                              (DATE_FORMAT.replace('%', '%%')) )
@@ -221,7 +318,13 @@ def main():
             analysis = get_spot_analysis(lines, pattern_spot)
             analysis_spot[pattern_spot] = analysis
 
-    display_graph(analysis_density, analysis_spot, args.title)
+    analysis_value = {}
+    if args.value:
+        for pattern_value in args.value:
+            analysis = get_value_analysis(lines, pattern_value)
+            analysis_value[pattern_value] = analysis
+
+    display_graph(args.title, analysis_density, analysis_spot, analysis_value)
 
     
 
